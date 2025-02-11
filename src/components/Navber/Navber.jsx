a57560085e78207b1745e72d7f3a002865fb1c15
Mahidishikder <mahidishikder@gmail.com>
2025-02-11 20:34:17 +0600

    navber work this profesonal done

--- src/components/Navber/Navber.jsx
+++ src/components/Navber/Navber.jsx
@@ -1,158 +1,226 @@
-import { useContext } from "react";
-import { HiMenuAlt1 } from "react-icons/hi";
-import { Link, NavLink, useNavigate } from "react-router-dom";
+import { useContext, useState } from "react";
+import { HiMenuAlt1, HiX } from "react-icons/hi";
+import { NavLink, Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../provider/AuthPorvider";
 import { toast } from "react-toastify";
+import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
+import { FiPhoneCall } from "react-icons/fi";
 
 function Navber() {
-  const navigate = useNavigate()
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false); // State to toggle modal
+  const navigate = useNavigate();
   const { user, singOutUser } = useContext(AuthContext);
-  const profile = user && user?.photoURL
- 
+  const profile = user && user?.photoURL;
 
   const handleLogout = () => {
     singOutUser()
       .then(() => {
-        toast.success("Logout Successfull", {
+        toast.success("Logout Successful", {
           position: "top-right",
           autoClose: 1000,
         });
-      })
-      navigate('/login')
+      });
+    navigate("/login");
   };
 
+  const handleMenuToggle = () => setIsModalOpen(!isModalOpen); // Toggle modal state
+
   const links = (
-    <div className="space-x-8">
+    <>
       <NavLink
-        to={`/`}
+        to="/"
         className={({ isActive }) =>
-          isActive
-            ? "mx-2 py-2 font-medium underline underline-offset-8 rounded text-lg text-[#0075FF]"
-            : "mx-2 py-2 font-medium hover:underline-offset-8 rounded text-lg"
+          isActive ? "nav-link text-red-500" : "nav-link"
         }
+        onClick={handleMenuToggle} // Close the modal when a menu item is clicked
       >
         Home
       </NavLink>
-
-      
-      {
-        user && 
+      {user && (
         <>
-                <NavLink
-        to={`books`}
-        className={({ isActive }) =>
-          isActive
-            ? "mx-2 py-2 font-medium underline underline-offset-8 rounded text-lg text-[#0075FF]"
-            : "mx-2 py-2 font-medium hover:underline-offset-8 rounded text-lg"
-        }
-      >
-        Books
-      </NavLink>
-      <NavLink
-        to={`addBook`}
-        className={({ isActive }) =>
-          isActive
-            ? "mx-2 py-2 font-medium underline underline-offset-8 rounded text-lg text-[#0075FF]"
-            : "mx-2 py-2 font-medium hover:underline-offset-8 rounded text-lg"
-        }
-      >
-        Add Book
-      </NavLink>
-      <NavLink
-        to={`borrowedBooks`}
-        className={({ isActive }) =>
-          isActive
-            ? "mx-2 py-2 font-medium underline underline-offset-8 rounded text-lg text-[#0075FF]"
-            : "mx-2 py-2 font-medium hover:underline-offset-8 rounded text-lg"
-        }
-      >
-        Borrowed Books
-      </NavLink>
-
-     
+          <NavLink
+            to="books"
+            className={({ isActive }) =>
+              isActive ? "nav-link text-red-500" : "nav-link"
+            }
+            onClick={handleMenuToggle}
+          >
+            Books
+          </NavLink>
+          <NavLink
+            to="addBook"
+            className={({ isActive }) =>
+              isActive ? "nav-link text-red-500" : "nav-link"
+            }
+            onClick={handleMenuToggle}
+          >
+            Add Book
+          </NavLink>
+          <NavLink
+            to="borrowedBooks"
+            className={({ isActive }) =>
+              isActive ? "nav-link text-red-500" : "nav-link"
+            }
+            onClick={handleMenuToggle}
+          >
+            Borrowed Books
+          </NavLink>
         </>
-      }
-       <NavLink
-        to={`about`}
+      )}
+      <NavLink
+        to="about"
         className={({ isActive }) =>
-          isActive
-            ? "mx-2 py-2 font-medium underline underline-offset-8 rounded text-lg text-[#0075FF]"
-            : "mx-2 py-2 font-medium hover:underline-offset-8 rounded text-lg"
+          isActive ? "nav-link text-red-500" : "nav-link"
         }
+        onClick={handleMenuToggle}
       >
         About Us
       </NavLink>
-
       <NavLink
-        to={`contact`}
+        to="contact"
         className={({ isActive }) =>
-          isActive
-            ? "mx-2 py-2 font-medium underline underline-offset-8 rounded text-lg text-[#0075FF]"
-            : "mx-2 py-2 font-medium hover:underline-offset-8 rounded text-lg"
+          isActive ? "nav-link text-red-500" : "nav-link"
         }
+        onClick={handleMenuToggle}
       >
         Contact Us
       </NavLink>
-
-    </div>
+    </>
   );
 
   return (
-    <div className="bg-gray-100">
-      <div className="navbar max-w-7xl mx-auto  py-4">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="text-2xl sm:text-3xl lg:hidden">
-            <HiMenuAlt1 />
+    <div>
+      {/* Top Section */}
+      <div className="bg-[#EF2346] text-white py-2 border-b border-white/20">
+        <div className="max-w-screen-2xl mx-auto flex flex-wrap justify-between items-center px-4 md:px-8">
+          <h3 className="text-white/90 text-sm md:text-base font-semibold">
+            Welcome to Pathmala.com!
+          </h3>
+          <div className="flex items-center gap-6">
+            <span className="flex items-center gap-2 text-white/90 text-sm md:text-base">
+              <FiPhoneCall size={20} className="text-yellow-300" />
+              +880 1234 567890
+            </span>
+            <div className="flex gap-3">
+              <a
+                href="#"
+                className="hover:text-yellow-300 transition-transform duration-200 hover:scale-110"
+              >
+                <FaFacebookF size={20} />
+              </a>
+              <a
+                href="#"
+                className="hover:text-yellow-300 transition-transform duration-200 hover:scale-110"
+              >
+                <FaTwitter size={20} />
+              </a>
+              <a
+                href="#"
+                className="hover:text-yellow-300 transition-transform duration-200 hover:scale-110"
+              >
+                <FaInstagram size={20} />
+              </a>
+            </div>
           </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-          >
-            {links}
-          </ul>
         </div>
-        <div className="flex items-center cursor-pointer">
+      </div>
+
+      {/* Navbar */}
+      <div className="bg-gray-200">
+        <div className="navbar max-w-screen-2xl mx-auto py-4">
+          <div className="navbar-start">
+            <div className="flex items-center cursor-pointer">
+              <img src="https://i.ibb.co/7L39PBQ/Liceria.png" alt="Logo" />
+            </div>
+          </div>
+
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1 space-x-10 font-medium text-lg">
+              {links}
+            </ul>
+          </div>
+
+          <div className="navbar-end">
+            {user ? (
+              <div className="flex gap-2 items-center">
+                <img
+                  className="cursor-pointer lg:w-10 lg:h-10 w-9 h-9 rounded-full"
+                  src={profile}
+                  alt="Profile"
+                />
+                <button
+                  onClick={handleLogout}
+                  className="rounded font-medium text-white/80 md:text-lg py-2 px-3 bg-red-500"
+                >
+                  Logout
+                </button>
+              </div>
+            ) : (
+              <div className="flex items-center gap-4">
+                <Link to="/login">
+                  <a className="hover:ring-[#EF2346] text-white hover:bg-white bg-[#EF2346] hover:ring-1 py-2 px-4 rounded font-medium cursor-pointer hover:text-[#000000] duration-300">
+                    Login
+                  </a>
+                </Link>
+                <div className="dropdown">
+                  <div
+                    tabIndex={0}
+                    role="button"
+                    className="text-3xl sm:text-3xl lg:hidden"
+                    onClick={handleMenuToggle}
+                  >
+                    {isMenuOpen ? <HiX /> : <HiMenuAlt1 />}
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+
+      {/* Modal for Menu */}
+      <div
+        className={`fixed top-0 right-0 h-full w-[100%] bg-white shadow-lg p-4 z-20 transform transition-all duration-300 ease-in-out ${
+          isModalOpen ? "translate-x-0" : "translate-x-full"
+        }`}
+      >
+        <div className="flex justify-end">
+          <button onClick={handleMenuToggle} className="text-2xl">
+            <HiX />
+          </button>
+        </div>
+        <div>
           <img
-            className="w-10"
-            src="https://img.icons8.com/?size=80&id=OYbXGW7sJiOG&format=png"
+            className="mb-10 mx-auto"
+            src="https://i.ibb.co.com/Mq5LpJ5/Liceria-1.png"
             alt=""
           />
-          <a className="text-2xl font-bold">PATHMALA</a>
+        </div>
+        <div className="flex flex-col items-center space-y-6 font-medium text-lg">
+          {links}
+        </div>
+        <div className="flex gap-3 justify-center mt-10">
+          <a
+            href="#"
+            className="hover:text-yellow-300 transition-transform duration-200 hover:scale-110"
+          >
+            <FaFacebookF size={20} />
+          </a>
+          <a
+            href="#"
+            className="hover:text-yellow-300 transition-transform duration-200 hover:scale-110"
+          >
+            <FaTwitter size={20} />
+          </a>
+          <a
+            href="#"
+            className="hover:text-yellow-300 transition-transform duration-200 hover:scale-110"
+          >
+            <FaInstagram size={20} />
+          </a>
         </div>
       </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">{links}</ul>
-      </div>
-
-
-
-
-      <div className="navbar-end">
-          {
-            user?
-            <div className="flex gap-2 items-center">
-            <img  className=" cursor-pointer lg:w-10 lg:h-10 w-9 h-9 rounded-full" src={profile} alt="" />
-           <button
-             onClick={handleLogout}
-             className="rounded font-medium text-white/80 md:text-lg py-2 px-3 bg-red-500"
-           >
-             Logout
-           </button>
-          </div>
-            : 
-            <Link to={`/login`}>
-            <a className="hover:ring-[#0075FF] text-white hover:bg-white bg-[#0075FF] hover:ring-1 py-2 px-4 rounded font-medium cursor-pointer hover:text-[#0075FF] duration-300">
-              Login
-            </a>
-          </Link>
-          }
-       
-          
-          
-        
-      </div>
-    </div>
     </div>
   );
 }
--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,88 +1,53 @@
-import { motion } from 'framer-motion';
+import React from 'react';
+import './style.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/autoplay';
+import { Pagination, Autoplay } from 'swiper/modules';
 
 function Banner() {
   return (
-    <div className="carousel w-full">
-      {/* Slide 1 */}
-      <div id="slide1" className="carousel-item relative w-full">
-        <img
-          src="https://plus.unsplash.com/premium_photo-1721762724233-1332468b252f?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          alt="Library Image"
-          className="w-full h-[700px] object-cover"
-        />
-        <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white px-4 sm:px-6 lg:px-8">
-          <div className="text-center text-white/80">
-            <motion.h1
-             initial={{ x: '-100%' }}  
-             animate={{ x: 0 }}        
-             transition={{ duration: 2 }}
-             className="text-4xl sm:text-5xl font-bold">Explore a World of Books at Our Library</motion.h1>
-            <motion.p
-            initial={{ x: '100%' }}  
-            animate={{ x: 0 }}       
-            transition={{ duration: 2 }}
-             className="mt-6 text-lg sm:text-xl">Welcome to our digital library, where knowledge, adventure, and imagination come alive. Browse through a wide collection of books across <br /> genres and discover new worlds, stories, and ideas.</motion.p>
-            <div className="mt-8">
-              <motion.a
-               initial={{ opacity: 0 }}  
-               animate={{ opacity: 1 }}  
-               transition={{ duration: 2 }}
-               href="#slide2" className="bg-[#0075FF] py-3 px-14 rounded mx-2">Book Borrow</motion.a>
-            </div>
-          </div>
-        </div>
-        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a href="#slide3" className="btn btn-circle">❮</a>
-          <a href="#slide2" className="btn btn-circle">❯</a>
-        </div>
-      </div>
-
-      {/* Slide 2 */}
-      <div id="slide2" className="carousel-item relative w-full">
-        <img
-          src="https://images.unsplash.com/photo-1524578271613-d550eacf6090?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          alt="Library Image"
-          className="w-full h-[700px] object-cover"
-        />
-        <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white px-4 sm:px-6 lg:px-8">
-          <div className="text-center text-white/80">
-            <h1 className="text-4xl sm:text-5xl font-bold">Unleash Your Imagination with Our Library</h1>
-            <p className="mt-6 text-lg sm:text-xl">Dive into a world of endless possibilities with our digital library. With an ever-growing collection of books across all genres, we offer something for every reader. <br /> From educational resources to imaginative tales.</p>
-            <div className="mt-8">
-              <a href="#slide3" className="bg-[#0075FF] py-3 px-14 rounded mx-2">Learn More</a>
-            </div>
-          </div>
-        </div>
-        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a href="#slide1" className="btn btn-circle">❮</a>
-          <a href="#slide3" className="btn btn-circle">❯</a>
-        </div>
-      </div>
-
-      {/* Slide 3 */}
-      <div id="slide3" className="carousel-item relative w-full">
-        <img
-          src="https://images.unsplash.com/photo-1589829085413-56de8ae18c73?q=80&w=2112&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          alt="Library Image"
-          className="w-full h-[700px] object-cover"
-        />
-        <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white px-4 sm:px-6 lg:px-8">
-          <div className="text-center text-white/80">
-            <h1 className="text-4xl sm:text-5xl font-bold">Discover New Horizons in Our Library</h1>
-            <p className="mt-6 text-lg sm:text-xl">Step into a realm where every page turns into an adventure. Our digital library offers a diverse collection of books, from timeless classics to contemporary  <br />  masterpieces.Whether you're seeking knowledge.</p>
-            <div className="mt-8">
-              <a href="#slide1" className="bg-[#0075FF] py-3 px-14 rounded mx-2">Learn More</a>
-            </div>
-          </div>
-        </div>
-        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a href="#slide2" className="btn btn-circle">❮</a>
-          <a href="#slide1" className="btn btn-circle">❯</a>
-        </div>
-      </div>
+    <div className="banner-container">
+      <Swiper
+        pagination={{ clickable: true }}
+        modules={[Pagination, Autoplay]}
+        autoplay={{ delay: 2000, disableOnInteraction: false }} // 2 sec delay
+        speed={1500} // Smooth transition (1.5 sec)
+        className="swiper"
+      >
+        <SwiperSlide>
+          <img 
+            src="https://www.bookswagon.com/bannerimages/70_inr.jpg?v=3.5" 
+            alt="Slide 1"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img 
+            src="https://www.bookswagon.com/bannerimages/85_inr.jpg?v=3.5" 
+            alt="Slide 2"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img 
+            src="https://www.bookswagon.com/bannerimages/32_inr.jpg?v=1.4" 
+            alt="Slide 3"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img 
+            src="https://www.bookswagon.com/bannerimages/79_inr.jpg?v=3.5" 
+            alt="Slide 4"
+          />
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 }
 
 export default Banner;
+
+
 
