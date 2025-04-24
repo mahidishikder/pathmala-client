dcb969ba9c67057c180692c60b118159bb994922
Mahidi <mahidishikder@Mahidis-MacBook-Air.local>
2025-04-24 11:27:02 +0600

    profile

--- src/pages/Loading/Loading.jsx
+++ src/pages/Loading/Loading.jsx
@@ -1,7 +1,7 @@
 
 function Loading() {
   return (
-    <div className="my-96  max-w-7xl mx-auto flex justify-center items-center">
+    <div className="py-96 bg-gray-100 max-w-7xl mx-auto flex justify-center items-center">
      <span className="bg-[#EF2346] loading loading-ball loading-lg"></span>
      <span className="bg-[#EF2346] loading loading-ball loading-lg"></span>
      <span className="bg-[#EF2346] loading loading-ball loading-lg"></span>
